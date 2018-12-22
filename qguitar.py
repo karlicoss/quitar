0b403c635f2bb8e2bec8a4bce10e03f4f5b2a4ab
Dima Gerasimov <karlicoss@gmail.com>
2018-12-22 02:07:47 +0000

    speedup animation

--- qguitar.py
+++ qguitar.py
@@ -2,6 +2,7 @@
 from sympy import symbols # type: ignore
 from sympy import Piecewise, log, ITE, piecewise_fold, integrate, sin, cos, pi
 # from sympy.abc import x, y # type: ignore
+import numpy as np
 
 # f = x**2
 # g = log(x)
@@ -18,7 +19,8 @@ f = Piecewise(
 
     # (x    , x <= L/4),
     # (L/4-x, x <= L/2),
-    (sin(pi / (L / 2) * x), x <= L/2),
+    # (sin(pi / (L / 2) * x), x <= L/2),
+    (sin(pi / L * x), x <= L),
 
     (0    , True),
 )
@@ -26,64 +28,126 @@ f = Piecewise(
 # print(f.subs(0))
 # print(f.subs(x, 1))
 
-def A(n):
+def fA(n):
     return 2 * integrate(f * sin(pi * n * x), (x, 0, L))
 
-def B(n):
+def fB(n):
     return 0.0
 
-N = 5
+N = 1
 Ns = range(1, N + 1)
-a = [0] + [A(n) for n in Ns]
-b = [0] + [B(n) for n in Ns]
+A = [fA(n) for n in Ns]
+B = [fB(n) for n in Ns]
+# TODO test with phone?...
 
 # print(a)
 
 # TODO shit, that can be analytical right?...
 
-u = sum([(a[n] * cos(pi * n * t) + b[n] * sin(pi * n * t)) * sin(pi * n * x) for n in Ns])
-# print(u)
-# 
-# # def u(x, t):
-# #     return (f(x - c * t) + f(x + c * t)) / 2
+# TODO ok, fund. frequency is implicitly 1 here (the small w)
 
-# plots = [plot(u(x, tt), (x, 0, L), ymax=1, ymin=-1) for tt in sxrange(0, Tmax, Tmax/steps)]
-# plots = [plot(x ** 2 * tt, (x, 0, L), ymax=10) for tt in sxrange(0, 10.0, 1.0)]
+C = [a * cos(pi * n * t) + b * sin(pi * n * t) for n, a, b in zip(Ns, A, B)]
+from sympy import lambdify
+CC = [lambdify(t, c, "numpy") for c in C]
 
-# plots = [plot(lambda xx,tt=tt: u(xx, tt), 0, L, ymax=1, ymin=-1) for tt in sxrange(0, Tmax, Tmax/steps)]
-# a = animate(plots)
-# a.show(delay=20, iterations=steps)
-# 
-# print(p)
-Tmax = 4
+u = sum([c * sin(pi * n * x) for c, n in zip(C, Ns)])
+
+u_fast = lambdify([t, x], u, "numpy")
+
+Tmax = 6
 steps = 100
 points = 50
 
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
-fig = plt.figure()
-plt.xlim(0-0.1, L + 0.1)
-plt.ylim(-AA-0.1, AA+0.1)
-# TODO multiple threads?..
-ims = []
-# TODO ugh
-for tt in np.arange(0.0, Tmax, Tmax/steps):
-    print(f'processing {tt}')
-    ut = u.subs(t, tt)
+def music():
+    M = 126
+    mvol = M / len(CC)
 
-    xs = np.linspace(0.0, L, points)
-    us = [ut.subs(x, xx) for xx in xs]
-    # print(tt)
-    pp = plt.plot(xs, us)
-    ims.append(pp)
-    # TODO wonder if we can do it in realtime??
-    # calc the fourier coeff; play sound
-    # ims.append((plt.pcolor(x, y, base + add, norm=plt.Normalize(0, 30)),))
+    seconds = 5
+    Fs = 44100
+    samples = Fs * seconds
 
-im_ani = animation.ArtistAnimation(fig, ims, interval=200, blit=True) # , repeat_delay=3000, blit=True)
-# To save this second animation with some metadata, use the following command:
-# im_ani.save('im.mp4', metadata={'artist':'Guido'})
 
-plt.show()
+    w = 2 * np.pi / Fs
+
+    wav = []
+
+    F = 440
+    for s in range(samples):
+        if s % 1000 == 0:
+            print(s)
+        # TODO shit. too slow?
+        tt = s / Fs
+        wv = mvol * np.sum([c(tt) * np.sin(s * w * n * F) for n, c in zip(Ns, CC)])
+        wav.append(wv)
+
+    import struct
+    with open('test.wav', 'wb') as fo:
+        for i in wav:
+            i = 128 + int(i)
+            fo.write(struct.pack('B', i))
+
+    import subprocess
+    subprocess.run([
+        'aplay',
+        '-f', 'U8',
+        '-r' + str(Fs),
+        'test.wav',
+       ])
+# basically, c[n](t) is the amplitude of nth fund frequency at time t
+
+def do_plots():
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
+
+    # TODO multiple threads?..
+    ims = []
+    # TODO ugh
+   #  for tt in np.arange(0.0, Tmax, Tmax/steps):
+   #      print(f'processing {tt}')
+
+   #      xs = np.linspace(0.0, L, points)
+   #      us = [ut.subs(x, xx) for xx in xs]
+   #      # print(tt)
+   #      pp = plt.plot(xs, us, title=f'{tt}')
+   #      ims.append(pp)
+        # TODO wonder if we can do it in realtime??
+        # calc the fourier coeff; play sound
+        # ims.append((plt.pcolor(x, y, base + add, norm=plt.Normalize(0, 30)),))
+
+    # im_ani = animation.ArtistAnimation(fig, ims, interval=200, blit=True) # , repeat_delay=3000, blit=True)
+    # To save this second animation with some metadata, use the following command:
+    # im_ani.save('im.mp4', metadata={'artist':'Guido'})
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from matplotlib.animation import FuncAnimation
+
+    fig, ax = plt.subplots()
+    xdata, ydata = [], []
+    ln, = plt.plot([], [], 'r.', animated=True)
+    time_template = 'time = %.1fs'
+    time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+    def init():
+        ax.set_xlim(0-0.1, L + 0.1)
+        ax.set_ylim(-AA-0.1, AA+0.1)
+        return ln,
+
+    def update(tt):
+        # TODO pythonlise?..
+        # TODO need to erase first?..
+        xs = np.linspace(0.0, L, points)
+        ys = u_fast(tt, xs)
+
+        ax.set_title(f'hi {tt}')
+        ln.set_data(xs, ys)
+        time_text.set_text(time_template % (tt))
+        return ln, time_text
+
+    ani = FuncAnimation(fig, update, frames=np.linspace(0, Tmax, steps), init_func=init, blit=True, interval=100)
+    plt.show()
+
+
+# music()
+do_plots()
